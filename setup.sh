e97ad954198e713dac00a03941c64e37dff169eb
TidalPaladin <tidalpaladin@gmail.com>
2016-02-25 15:18:34 -0600

    setup changes

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 apt-get install -y apache2 php5 network-manager fbi unclutter matchbox xorg xserver-xorg x11-xserver-utils php5-curl luakit
 chown -R :www-data /var/www/html
-chmod -R 754 /var/www/html
+chmod -R 755 /var/www/html
 chmod -R 770 /var/www/html/server_files
-cp /var/www/html/server_files/xinitrc /boot/xinitrc
-cp /var/www/html/server_files/rc.local /etc/rc.local
+cp /var/www/html/system_files/xinitrc /boot
+cp /var/www/html/system_files/rc.local /etc
