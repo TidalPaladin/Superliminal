76625316f1c5784ec3ac80febd390221bc3ba6f0
TidalPaladin <tidalpaladin@gmail.com>
2016-02-25 15:10:53 -0600

    Permission change

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apt-get install -y apache2 php5 network-manager fbi unclutter matchbox-window-man xorg xserver-xorg php5-curl luakit
+apt-get install -y apache2 php5 network-manager fbi unclutter matchbox xorg xserver-xorg x11-xserver-utils php5-curl luakit
 chown -R :www-data /var/www/html
-chmod -R 750 /var/www/html
+chmod -R 754 /var/www/html
 chmod -R 770 /var/www/html/server_files
 cp /var/www/html/server_files/xinitrc /boot/xinitrc
 cp /var/www/html/server_files/rc.local /etc/rc.local
